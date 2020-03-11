79d546126b1df608177b607d7eccf2aea9e80a2e
Domenico Cascone <dcascone84@gmail.com>
2020-03-11 21:30:38 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,8 @@ def my_find(collection)
   i = 0
   block_return_value = []
   while i < collection.length
-    yield(collection[i])
+    block_return_value << yield(collection[i])
     i += 1
   end
+  block_return_value
 end
